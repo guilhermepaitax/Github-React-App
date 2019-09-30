17a516b7339eb4475b5923465a29972def60ce92
Guilherme Paitax <guilhermepaitax16@gmail.com>
2019-09-30 16:16:35 -0300

    Editando shimmer effect

--- src/components/Line/index.js
+++ src/components/Line/index.js
@@ -3,13 +3,14 @@ import styled from 'styled-components';
 export const Line = styled.div`
   height: 17px;
   width: ${props => props.width || '70%'};
-  margin-top: 15px;
+  margin-left: ${props => props.marginLeft || '0'};
+  margin-top: ${props => props.marginTop || '15px'};
   border-radius: 4px;
 `;
 
 export const Circle = styled.div`
-  width: 110px;
-  height: 110px;
+  width: ${props => props.size || '110px'};
+  height: ${props => props.size || '110px'};
   border-radius: 50%;
   margin-top: 20px;
 `;
